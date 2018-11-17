ac133facf6f902e68870bc84b3a9c780ae402c2d
tickle <qu7.52m@gmail.com>
2018-11-17 09:08:16 +0900

    [ver0.66.0] spanタグ適用時に色も指定できるよう変更

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -8,7 +8,7 @@
  * 
  * https://github.com/cwtickle/danoniplus
  */
-var g_version = "Ver 0.65.1";
+var g_version = "Ver 0.66.0";
 
 // ショートカット用文字列(↓の文字列を検索することで対象箇所へジャンプできます)
 //  タイトル:melon  設定・オプション:lime  キーコンフィグ:orange  譜面読込:strawberry  メイン:banana  結果:grape
