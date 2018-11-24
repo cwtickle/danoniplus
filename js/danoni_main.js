7e32b5f30f5d9103b895257d27883bbd15535d17
tickle <qu7.52m@gmail.com>
2018-11-24 22:39:08 +0900

    [ver0.77.0] リザルト画面のデザイン変更

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -8,8 +8,7 @@
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = "Ver 0.76.3";
-const g_version_result = "Ver 0.1.0.20181124";
+const g_version = "Ver 0.77.0";
 
 // ショートカット用文字列(↓の文字列を検索することで対象箇所へジャンプできます)
 //  タイトル:melon  設定・オプション:lime  キーコンフィグ:orange  譜面読込:strawberry  メイン:banana  結果:grape
