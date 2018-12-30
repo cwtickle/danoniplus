16713ed281b2f8bc91e949bc2758760292892e1a
tickle <qu7.52m@gmail.com>
2018-12-30 09:21:21 +0900

    [ver1.9.0] 歌詞機能の強化（表示箇所拡張、中央寄せ・右寄せ実装　他）

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,11 +4,11 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2018/12/25
+ * Revised : 2018/12/30
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = "Ver 1.8.0";
+const g_version = "Ver 1.9.0";
 const g_version_gauge = "Ver 0.5.1.20181223";
 const g_version_musicEncoded = "Ver 0.1.1.20181224";
 const g_version_lyrics = "Ver 0.2.0.20181230";
