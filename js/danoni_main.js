3e659972bf44391382ccdd39ddbe81a9a293a008
tickle <qu7.52m@gmail.com>
2019-02-17 15:53:40 +0900

    [ver2.9.0] ロード画面をカスタムできるように変更

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -8,7 +8,7 @@
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 2.8.1`;
+const g_version = `Ver 2.9.0`;
 const g_revisedDate = `2019/02/17`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
