3aefb3e3fdf1f5f917a8d8bba82bccffd405c72b
tickle <qu7.52m@gmail.com>
2019-03-01 20:53:41 +0900

    [ver3.1.4] 追加キー定義でシャッフルグループが正しく読み込めない問題の修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2019/02/27
+ * Revised : 2019/03/01
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 3.1.3`;
-const g_revisedDate = `2019/02/27`;
+const g_version = `Ver 3.1.4`;
+const g_revisedDate = `2019/03/01`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
