ca7e0e4352e12456a54f5c55bef438217e7e50ee
tickle <qu7.52m@gmail.com>
2019-04-03 22:49:41 +0900

    [ver3.6.0] 設定ファイルのデフォルト値を指定　他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2019/03/23
+ * Revised : 2019/04/03
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 3.5.0`;
-const g_revisedDate = `2019/03/23`;
+const g_version = `Ver 3.6.0`;
+const g_revisedDate = `2019/04/03`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
