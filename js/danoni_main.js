549e4f1537a3d311c25adc94380e090266f59d56
tickle <qu7.52m@gmail.com>
2019-04-21 00:27:36 +0900

    [ver3.12.0] 矢印色の基本値を選択できるように変更 (ColorTypeの追加)

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2019/04/17
+ * Revised : 2019/04/21
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 3.11.1`;
-const g_revisedDate = `2019/04/17`;
+const g_version = `Ver 3.12.0`;
+const g_revisedDate = `2019/04/21`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
