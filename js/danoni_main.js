1798e3178ac86447c8204a02cdc6bff05ee4ffa5
tickle <qu7.52m@gmail.com>
2019-05-02 21:26:51 +0900

    [ver4.4.1] Volumeのカーソル位置をLocalStorageから読み込んだ値に合わせるように修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2019/05/01
+ * Revised : 2019/05/02
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 4.4.0`;
-const g_revisedDate = `2019/05/01`;
+const g_version = `Ver 4.4.1`;
+const g_revisedDate = `2019/05/02`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
