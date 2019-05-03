01eb1ab5184506e4c16e92819b6da9880e8f568e
tickle <qu7.52m@gmail.com>
2019-05-03 21:35:33 +0900

    [ver4.6.0] localStorage保存有無ボタン追加、キー別のlocalStorage名を変更 他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -8,7 +8,7 @@
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 4.5.0`;
+const g_version = `Ver 4.6.0`;
 const g_revisedDate = `2019/05/03`;
 const g_alphaVersion = ``;
 
