c594e2f6cef39d626dc9ff79be11a0cc1b04b83d
tickle <qu7.52m@gmail.com>
2019-05-05 09:09:41 +0900

    [ver4.7.0] backtitle_dataのループ、フレームジャンプに対応

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2019/05/04
+ * Revised : 2019/05/05
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 4.6.2`;
-const g_revisedDate = `2019/05/04`;
+const g_version = `Ver 4.7.0`;
+const g_revisedDate = `2019/05/05`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
