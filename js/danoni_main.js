507872d5d843f51c9dfdd70ad11fc67ce4a0ff0e
tickle <qu7.52m@gmail.com>
2019-05-26 17:29:03 +0900

    [ver5.4.0] 歌詞表示(word_data)の複数階層化対応 #307

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -8,7 +8,7 @@
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 5.3.0`;
+const g_version = `Ver 5.4.0`;
 const g_revisedDate = `2019/05/26`;
 const g_alphaVersion = ``;
 
