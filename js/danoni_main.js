57c6a109ad01f12d51e08272ddce892bc0cf495c
tickle <qu7.52m@gmail.com>
2019-05-30 22:47:33 +0900

    [ver5.6.5] 歌詞表示のフェードイン・アウトのモーションをlinearに変更

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2019/05/28
+ * Revised : 2019/05/30
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 5.6.2`;
-const g_revisedDate = `2019/05/28`;
+const g_version = `Ver 5.6.5`;
+const g_revisedDate = `2019/05/30`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
