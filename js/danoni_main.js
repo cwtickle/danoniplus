3b88523fa3fccdb92394f28677a99448930908c1
tickle <qu7.52m@gmail.com>
2019-06-26 23:18:02 +0900

    [ver6.2.1] 独自キーが2譜面目以降にある場合に読み込みできない場合がある問題を修正　他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2019/06/23
+ * Revised : 2019/06/26
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 6.2.0`;
-const g_revisedDate = `2019/06/23`;
+const g_version = `Ver 6.2.1`;
+const g_revisedDate = `2019/06/26`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
