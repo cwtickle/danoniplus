6e66f821c0d73bcccea7a7bba5a8639dac026e94
tickle <qu7.52m@gmail.com>
2019-06-30 00:01:51 +0900

    [ver6.5.0] ライフ制ゲージの可変対応、ライフ上限値設定

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2019/06/28
+ * Revised : 2019/06/30
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 6.4.0`;
-const g_revisedDate = `2019/06/28`;
+const g_version = `Ver 6.5.0`;
+const g_revisedDate = `2019/06/30`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
