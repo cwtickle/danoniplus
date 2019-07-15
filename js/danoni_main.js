dfe8a349f90c6c1da6b71bb7beaac69e4db035a2
tickle <qu7.52m@gmail.com>
2019-07-15 12:59:59 +0900

    [ver7.2.0] ボタンUIのアニメーション対応　他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2019/07/12
+ * Revised : 2019/07/15
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 7.1.1`;
-const g_revisedDate = `2019/07/12`;
+const g_version = `Ver 7.2.0`;
+const g_revisedDate = `2019/07/15`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
