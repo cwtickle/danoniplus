b69c7289f94bc88d72a47d9ab77e7637571ac5bb
tickle <qu7.52m@gmail.com>
2019-07-21 11:05:57 +0900

    [ver7.4.0] カラーコードの左パディング機能を実装　他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2019/07/18
+ * Revised : 2019/07/21
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 7.3.1`;
-const g_revisedDate = `2019/07/18`;
+const g_version = `Ver 7.4.0`;
+const g_revisedDate = `2019/07/21`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
