627c206ba9198354177f245eb435b7c31d5b7e41
tickle <qu7.52m@gmail.com>
2019-08-25 15:07:00 +0900

    [ver7.7.0] 読込画面移動時に譜面を再読込する仕様に変更　他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2019/08/18
+ * Revised : 2019/08/25
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 7.6.0`;
-const g_revisedDate = `2019/08/18`;
+const g_version = `Ver 7.7.0`;
+const g_revisedDate = `2019/08/25`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
