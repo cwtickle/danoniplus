9af36a1718440c388a93733a2c2d04aa4dc1af2a
tickle <qu7.52m@gmail.com>
2019-08-31 00:10:00 +0900

    [ver7.8.1] 位置調整によらず見た目の曲時間を表示するように変更

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2019/08/30
+ * Revised : 2019/08/31
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 7.8.0`;
-const g_revisedDate = `2019/08/30`;
+const g_version = `Ver 7.8.1`;
+const g_revisedDate = `2019/08/31`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
