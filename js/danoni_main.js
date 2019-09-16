ad577bc6000ab6136acea38d84d3fd58e0b879fd
tickle <qu7.52m@gmail.com>
2019-09-16 16:22:16 +0900

    [ver7.9.3] ver8.0.2の差分修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2019/09/15
+ * Revised : 2019/09/16
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 7.9.2`;
-const g_revisedDate = `2019/09/15`;
+const g_version = `Ver 7.9.3`;
+const g_revisedDate = `2019/09/16`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
