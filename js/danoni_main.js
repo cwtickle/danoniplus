7028890885e7217e52acf27ee4aaa2157cd6477f
tickle <qu7.52m@gmail.com>
2019-09-24 22:38:28 +0900

    [ver8.2.0] 曲名（複数行）を結合する場合の半角スペース抜きに対応 他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2019/09/23
+ * Revised : 2019/09/24
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 8.1.0`;
-const g_revisedDate = `2019/09/23`;
+const g_version = `Ver 8.2.0`;
+const g_revisedDate = `2019/09/24`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
