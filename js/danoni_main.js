674f283aee764d9a653523cceccfff06f09e16cd
tickle <qu7.52m@gmail.com>
2019-09-28 09:11:47 +0900

    [ver8.5.0] 譜面別にファイルを分割できる機能を実装

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2019/09/26
+ * Revised : 2019/09/28
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 8.4.0`;
-const g_revisedDate = `2019/09/26`;
+const g_version = `Ver 8.5.0`;
+const g_revisedDate = `2019/09/28`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
