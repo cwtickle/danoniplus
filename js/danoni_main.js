3607533c126c16653e848b0273a69b60157320b8
tickle <qu7.52m@gmail.com>
2019-09-30 22:57:19 +0900

    [ver8.6.1] エラーメッセージの不具合を修正　他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2019/09/29
+ * Revised : 2019/09/30
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 8.6.0`;
-const g_revisedDate = `2019/09/29`;
+const g_version = `Ver 8.6.1`;
+const g_revisedDate = `2019/09/30`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
