e0a81541bdb2b76d004178cab1996afb31b01451
tickle <qu7.52m@gmail.com>
2019-10-01 22:06:45 +0900

    [ver8.6.2] ゲージ設定が一部初期値になる問題を修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2019/09/30
+ * Revised : 2019/10/01
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 8.6.1`;
-const g_revisedDate = `2019/09/30`;
+const g_version = `Ver 8.6.2`;
+const g_revisedDate = `2019/10/01`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
