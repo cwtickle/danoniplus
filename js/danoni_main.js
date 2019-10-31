1e89b76a54482140e76563b01059a5a47f627295
tickle <qu7.52m@gmail.com>
2019-10-31 23:24:35 +0900

    [ver10.0.0-M1] オブジェクトの色部分をCSSへ分離

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2019/10/25
+ * Revised : 2019/10/31
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 9.4.3`;
-const g_revisedDate = `2019/10/21`;
+const g_version = `Ver 10.0.0-M1`;
+const g_revisedDate = `2019/10/31`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
