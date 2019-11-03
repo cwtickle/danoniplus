6695608f1b1558c3794156549ece60c88377e865
tickle <qu7.52m@gmail.com>
2019-11-03 23:20:18 +0900

    [ver10.0.0-RC] スキン:skyblue追加、設定画面のdiv要素集約　他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2019/11/02
+ * Revised : 2019/11/03
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 10.0.0-M2`;
-const g_revisedDate = `2019/11/02`;
+const g_version = `Ver 10.0.0-RC`;
+const g_revisedDate = `2019/11/03`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
