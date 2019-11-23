34eef4d50f4307e6a767ea4add4c45d21dd5c157
tickle <qu7.52m@gmail.com>
2019-11-23 11:20:44 +0900

    [ver10.2.2] 15A, 15B用のスクロール拡張設定誤りを修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2019/11/20
+ * Revised : 2019/11/23
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 10.2.1`;
-const g_revisedDate = `2019/11/20`;
+const g_version = `Ver 10.2.2`;
+const g_revisedDate = `2019/11/23`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
