8cb44432bffccc86aa0efc376d18d74278cb2219
tickle <qu7.52m@gmail.com>
2019-12-07 19:35:07 +0900

    [ver10.4.1] リザルト用ラベルの指定間違いを修正、コード整理

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -8,7 +8,7 @@
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 10.4.0`;
+const g_version = `Ver 10.4.1`;
 const g_revisedDate = `2019/12/07`;
 const g_alphaVersion = ``;
 
