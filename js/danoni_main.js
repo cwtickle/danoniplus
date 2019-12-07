0490b644747101dccb02f6b77454947c2996f14f
tickle <qu7.52m@gmail.com>
2019-12-07 14:20:46 +0900

    [ver10.4.0] コード整理＆矢印のヒット部分を押したタイミングに応じて変わるように変更

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2019/12/01
+ * Revised : 2019/12/07
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 10.3.0`;
-const g_revisedDate = `2019/12/01`;
+const g_version = `Ver 10.4.0`;
+const g_revisedDate = `2019/12/07`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
