109742d2ad64bf6cac4749dccd6ba07419e45b7d
tickle <qu7.52m@gmail.com>
2019-12-14 16:27:25 +0900

    [ver10.5.1] fileでも起動可能なようにcrossorigin属性の付加条件を変更

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2019/12/13
+ * Revised : 2019/12/14
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 10.5.0`;
-const g_revisedDate = `2019/12/13`;
+const g_version = `Ver 10.5.1`;
+const g_revisedDate = `2019/12/14`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
