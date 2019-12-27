ff5929d296a915e61b98e6884629266a1b6f038c
tickle <qu7.52m@gmail.com>
2019-12-28 07:14:55 +0900

    [ver11.1.2] 譜面ヘッダーのスペルミスを修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2019/12/27
+ * Revised : 2019/12/28
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 11.1.1`;
-const g_revisedDate = `2019/12/27`;
+const g_version = `Ver 11.1.2`;
+const g_revisedDate = `2019/12/28`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
