25e0064f533d6f7ba11c121f6b1da35a704c3dee
tickle <qu7.52m@gmail.com>
2020-01-03 23:19:55 +0900

    [ver11.2.0] キーブロックする処理の見直し　他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2019/12/28
+ * Revised : 2020/01/04
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 11.1.2`;
-const g_revisedDate = `2019/12/28`;
+const g_version = `Ver 11.2.0`;
+const g_revisedDate = `2020/01/04`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
