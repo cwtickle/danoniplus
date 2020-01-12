3bb85c51f468159a2d4ab86169e28014a837e40c
tickle <qu7.52m@gmail.com>
2020-01-12 22:19:07 +0900

    [ver11.2.1] コード見直し、judgement:OFFで判定部分が消えない問題を修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2020/01/04
+ * Revised : 2020/01/12
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 11.2.0`;
-const g_revisedDate = `2020/01/04`;
+const g_version = `Ver 11.2.1`;
+const g_revisedDate = `2020/01/12`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
