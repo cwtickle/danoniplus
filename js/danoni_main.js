1bdb7c05c95414d7746940bb1de903adbf5872d3
tickle <qu7.52m@gmail.com>
2020-02-25 22:29:43 +0900

    [ver12.1.1] スコア上書き時ハイスコア差分が計算されない問題を修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2020/02/24
+ * Revised : 2020/02/25
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 12.1.0`;
-const g_revisedDate = `2020/02/24`;
+const g_version = `Ver 12.1.1`;
+const g_revisedDate = `2020/02/25`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
