5beb5474fb642d17852f9529b9396b2f209519c4
tickle <qu7.52m@gmail.com>
2020-02-28 20:57:00 +0900

    [ver12.1.2] titlegrdが1行分だけ指定されているとき曲名の2行目が表示されない問題の修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2020/02/25
+ * Revised : 2020/02/28
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 12.1.1`;
-const g_revisedDate = `2020/02/25`;
+const g_version = `Ver 12.1.2`;
+const g_revisedDate = `2020/02/28`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
