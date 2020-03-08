58ff7534f3ccbfcc0b0fa370dd5642af60c28c10
tickle <qu7.52m@gmail.com>
2020-03-08 13:28:44 +0900

    [ver12.2.1] ダミー矢印のヒット位置が前の通常矢印に引きずられる問題を修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2020/03/06
+ * Revised : 2020/03/08
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 12.2.0`;
-const g_revisedDate = `2020/03/06`;
+const g_version = `Ver 12.2.1`;
+const g_revisedDate = `2020/03/08`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
