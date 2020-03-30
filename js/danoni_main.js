6b08fcdf15db3a8b8b3114a69f496ec4ecf1c19c
tickle <qu7.52m@gmail.com>
2020-03-30 22:33:12 +0900

    [ver13.1.0] 従来のfrzColor補完ができる設定を追加　他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2020/03/29
+ * Revised : 2020/03/30
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 13.0.0`;
-const g_revisedDate = `2020/03/29`;
+const g_version = `Ver 13.1.0`;
+const g_revisedDate = `2020/03/30`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
