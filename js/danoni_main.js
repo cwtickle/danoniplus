9deeb4e0bfc694c0b952a1f111c137be224df0bb
tickle <qu7.52m@gmail.com>
2020-04-12 10:54:23 +0900

    [ver13.3.1] キーコンフィグ画面でdeleteキーが使用できない問題を修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -8,7 +8,7 @@
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 13.3.0`;
+const g_version = `Ver 13.3.1`;
 const g_revisedDate = `2020/04/12`;
 const g_alphaVersion = ``;
 
