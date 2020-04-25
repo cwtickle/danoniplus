a3bb87e25ed92c75658e35318dc9d9d4e18c72fb
tickle <qu7.52m@gmail.com>
2020-04-25 16:10:51 +0900

    [ver13.6.0] start/fade/endFrameの疑似タイマー設定対応

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2020/04/16
+ * Revised : 2020/04/25
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 13.5.1`;
-const g_revisedDate = `2020/04/16`;
+const g_version = `Ver 13.6.0`;
+const g_revisedDate = `2020/04/25`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
