d083ccddcd5510e8f813d81f6bf4f9a994a23557
tickle <qu7.52m@gmail.com>
2020-04-29 13:57:15 +0900

    [ver14.0.2] displayChainOFFで同一値が指定された場合にエラーを返すよう変更　他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -8,7 +8,7 @@
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 14.0.1`;
+const g_version = `Ver 14.0.2`;
 const g_revisedDate = `2020/04/29`;
 const g_alphaVersion = ``;
 
--- js/lib/danoni_constants.js
+++ js/lib/danoni_constants.js
@@ -5,7 +5,7 @@
  *
  * Source by tickle
  * Created : 2019/11/19
- * Revised : 2020/04/28
+ * Revised : 2020/04/29
  *
  * https://github.com/cwtickle/danoniplus
  */
