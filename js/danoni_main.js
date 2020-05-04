a84c3db3ca0ea1d55a27f2357de1e2f540e2df72
tickle <qu7.52m@gmail.com>
2020-05-04 17:11:08 +0900

    [ver14.3.3] Fast/Slowカウントの誤りを修正　他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -8,7 +8,7 @@
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 14.3.2`;
+const g_version = `Ver 14.3.3`;
 const g_revisedDate = `2020/05/04`;
 const g_alphaVersion = ``;
 
