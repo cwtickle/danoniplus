32cdb1e5ef4b44d90323de93379e9a50a0eaf832
tickle <qu7.52m@gmail.com>
2020-06-18 23:33:09 +0900

    [ver15.2.1] キーコンフィグ保存時の問題を修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2020/06/13
+ * Revised : 2020/06/18
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 15.2.0`;
-const g_revisedDate = `2020/06/13`;
+const g_version = `Ver 15.2.1`;
+const g_revisedDate = `2020/06/18`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
