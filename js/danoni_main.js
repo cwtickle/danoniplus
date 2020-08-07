250ee36e0dab8ea83e3e58cf5f01050e778ca4ae
tickle <qu7.52m@gmail.com>
2020-08-07 09:22:24 +0900

    [ver16.0.1] Shift+Deleteキーの組み合わせが利かない問題を修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2020/08/06
+ * Revised : 2020/08/07
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 16.0.0`;
-const g_revisedDate = `2020/08/06`;
+const g_version = `Ver 16.0.1`;
+const g_revisedDate = `2020/08/07`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
