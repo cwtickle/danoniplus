7a049f4a6755fe7dee8eaf7e9a10d9ff8d480cac
tickle <qu7.52m@gmail.com>
2020-08-25 06:57:07 +0900

    [ver16.2.1] リザルトデータのフォーマット実装（再）

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2020/08/24
+ * Revised : 2020/08/25
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 16.2.0`;
-const g_revisedDate = `2020/08/24`;
+const g_version = `Ver 16.2.1`;
+const g_revisedDate = `2020/08/25`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
