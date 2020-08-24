6bda7f2a137c90000a248d3a0db4164bd936735c
tickle <qu7.52m@gmail.com>
2020-08-24 23:19:17 +0900

    [ver16.2.0] 20個以上のキーに対する色変化対応（単発矢印・個別）他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2020/08/22
+ * Revised : 2020/08/24
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 16.1.0`;
-const g_revisedDate = `2020/08/22`;
+const g_version = `Ver 16.2.0`;
+const g_revisedDate = `2020/08/24`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
