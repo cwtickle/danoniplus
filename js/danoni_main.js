75d917b82ae755b5982d247ba496ce2a1aa6f9a3
tickle <qu7.52m@gmail.com>
2020-09-05 17:58:57 +0900

    [ver16.3.0] Ready表示に関する拡張機能を実装

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2020/08/25
+ * Revised : 2020/09/05
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 16.2.1`;
-const g_revisedDate = `2020/08/25`;
+const g_version = `Ver 16.3.0`;
+const g_revisedDate = `2020/09/05`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
