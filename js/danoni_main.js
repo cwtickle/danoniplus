5a261a20d5586fdbc01342025e6e1f24a300764e
tickle <qu7.52m@gmail.com>
2020-10-09 20:26:28 +0900

    [0285/strict-code:0.3.0] var変数をconstへ変更

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -436,19 +436,19 @@ function paddingLeft(_str, _length, _chr) {
  */
 function copyTextToClipboard(_textVal) {
 	// テキストエリアを用意する
-	let copyFrom = document.createElement(`textarea`);
+	const copyFrom = document.createElement(`textarea`);
 	// テキストエリアへ値をセット
 	copyFrom.textContent = _textVal;
 
 	// bodyタグの要素を取得
-	var bodyElm = document.getElementsByTagName(`body`)[0];
+	const bodyElm = document.getElementsByTagName(`body`)[0];
 	// 子要素にテキストエリアを配置
 	bodyElm.appendChild(copyFrom);
 
 	// テキストエリアの値を選択
 	copyFrom.select();
 	// コピーコマンド発行
-	var retVal = document.execCommand(`copy`);
+	const retVal = document.execCommand(`copy`);
 	// 追加テキストエリアを削除
 	bodyElm.removeChild(copyFrom);
 	// 処理結果を返却
