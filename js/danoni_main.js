07eb9a08cb357cd0b0f21d82e39217ccbc1dd8b6
tickle <qu7.52m@gmail.com>
2020-10-09 21:51:43 +0900

    [ver17.4.0] strictモードを有効化　他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2020/10/08
+ * Revised : 2020/10/09
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 17.3.0`;
-const g_revisedDate = `2020/10/08`;
+const g_version = `Ver 17.4.0`;
+const g_revisedDate = `2020/10/09`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
