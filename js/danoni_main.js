d4aa4e3c9cae8671364ea2cfca9422ad304d1ec6
tickle <qu7.52m@gmail.com>
2020-10-11 00:02:23 +0900

    [ver17.4.2] strictモード有効に伴い未定義の変数を修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2020/10/10
+ * Revised : 2020/10/11
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 17.4.1`;
-const g_revisedDate = `2020/10/10`;
+const g_version = `Ver 17.4.2`;
+const g_revisedDate = `2020/10/11`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
