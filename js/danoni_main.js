15a19bbb4dd79db4984a2fb043b2af360a3d486d
tickle <qu7.52m@gmail.com>
2020-10-16 22:56:54 +0900

    [ver17.5.0] ボタン作成処理に右クリック処理、イベント引数を追加　他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2020/10/12
+ * Revised : 2020/10/16
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 17.4.3`;
-const g_revisedDate = `2020/10/12`;
+const g_version = `Ver 17.5.0`;
+const g_revisedDate = `2020/10/16`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
