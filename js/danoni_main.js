32e9844a98090b40951e768bc438c69b9cd4befe
tickle <qu7.52m@gmail.com>
2020-11-02 23:31:40 +0900

    [ver18.1.0] Display設定周りのローカルストレージ対応　他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2020/10/31
+ * Revised : 2020/11/02
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 18.0.1`;
-const g_revisedDate = `2020/10/31`;
+const g_version = `Ver 18.1.0`;
+const g_revisedDate = `2020/11/02`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
