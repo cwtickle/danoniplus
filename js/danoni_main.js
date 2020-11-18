09fdeaf2cfcde8903e0aa9968529b3a25f574000
tickle <qu7.52m@gmail.com>
2020-11-18 17:59:50 +0900

    [ver18.3.0] 背景・マスクのsvgファイル対応、ColorTypeのローカル保存実装　他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2020/11/08
+ * Revised : 2020/11/18
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 18.2.0`;
-const g_revisedDate = `2020/11/08`;
+const g_version = `Ver 18.3.0`;
+const g_revisedDate = `2020/11/18`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
