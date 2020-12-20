98a1d1f36e889f31e38e046246114a31748d35a5
tickle <qu7.52m@gmail.com>
2020-12-21 07:56:44 +0900

    [ver14.5.15] 曲終了直後にリトライキーを押すとALL0のリザルトが表示される問題の修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2020/11/21
+ * Revised : 2020/12/21
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 14.5.14`;
-const g_revisedDate = `2020/11/21`;
+const g_version = `Ver 14.5.15`;
+const g_revisedDate = `2020/12/21`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
@@ -8582,6 +8582,10 @@ function MainInit() {
 			if (g_stateObj.lifeMode === C_LFE_BORDER && g_workObj.lifeVal < g_workObj.lifeBorder) {
 				g_gameOverFlg = true;
 			}
+
+			document.onkeydown = evt => blockCode(transCode(evt.code));
+			document.onkeyup = evt => { }
+
 			clearTimeout(g_timeoutEvtId);
 			setTimeout(_ => {
 				clearWindow();
