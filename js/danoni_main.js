392a767e04877c29b3afbcd806ec83b6dd02911a
tickle <qu7.52m@gmail.com>
2020-12-21 18:23:32 +0900

    [ver18.6.0] ライフ制ゲージ設定にHeavyを追加　他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2020/12/20
+ * Revised : 2020/12/21
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 18.5.0`;
-const g_revisedDate = `2020/12/20`;
+const g_version = `Ver 18.6.0`;
+const g_revisedDate = `2020/12/21`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
