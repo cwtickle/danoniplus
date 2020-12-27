8e9549b33aa1d067378f0e5b76ba3f3fd8c39bae
tickle <qu7.52m@gmail.com>
2020-12-27 22:11:55 +0900

    [ver18.8.0] Gitter、ゲーム画面説明へのリンクを追加

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2020/12/25
+ * Revised : 2020/12/27
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 18.7.0`;
-const g_revisedDate = `2020/12/25`;
+const g_version = `Ver 18.8.0`;
+const g_revisedDate = `2020/12/27`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
--- js/lib/danoni_constants.js
+++ js/lib/danoni_constants.js
@@ -5,7 +5,7 @@
  *
  * Source by tickle
  * Created : 2019/11/19
- * Revised : 2020/12/25 (v18.7.0)
+ * Revised : 2020/12/27 (v18.8.0)
  *
  * https://github.com/cwtickle/danoniplus
  */
