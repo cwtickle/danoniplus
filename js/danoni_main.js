64300a3ada6ceffdf803e34c1a23f3f2e0d7d53c
tickle <qu7.52m@gmail.com>
2020-12-30 18:18:51 +0900

    [ver16.4.9] 一部のキーを押すと全押し状態になる問題を修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2020/12/25
+ * Revised : 2020/12/30
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 16.4.8`;
-const g_revisedDate = `2020/12/25`;
+const g_version = `Ver 16.4.9`;
+const g_revisedDate = `2020/12/30`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
