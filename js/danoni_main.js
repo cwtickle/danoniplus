2e9ec4ecd9b1225f7fd28d66feebf6ac147062cb
tickle <qu7.52m@gmail.com>
2021-02-14 14:51:39 +0900

    [ver20.1.1] 設定画面内の譜面名のMakerViewのフォントサイズ変更に対応

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2021/02/13
+ * Revised : 2021/02/14
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 20.1.0`;
-const g_revisedDate = `2021/02/13`;
+const g_version = `Ver 20.1.1`;
+const g_revisedDate = `2021/02/14`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
