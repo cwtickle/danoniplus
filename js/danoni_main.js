59a77c9c9b266a9dcdc1c5edc5638b80ea45b6c2
tickle <qu7.52m@gmail.com>
2021-05-16 00:19:31 +0900

    [ver21.5.1] 17keyType:2のシャッフルグループが間違っていたのを修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2021/04/22
+ * Revised : 2021/05/16
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 21.5.0`;
-const g_revisedDate = `2021/04/22`;
+const g_version = `Ver 21.5.1`;
+const g_revisedDate = `2021/05/16`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
