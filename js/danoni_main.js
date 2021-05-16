b87c0176dd2326bb1c0afaa209acd1475d912846
tickle <qu7.52m@gmail.com>
2021-05-16 15:15:44 +0900

    [ver22.4.1] カラーグループの変更機能実装、キーコンフィグ画面のデザイン微調整　他（再、文言修正）

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -8,7 +8,7 @@
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 22.4.0`;
+const g_version = `Ver 22.4.1`;
 const g_revisedDate = `2021/05/16`;
 const g_alphaVersion = ``;
 
