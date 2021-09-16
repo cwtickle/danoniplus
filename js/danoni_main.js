55717b1385832b50bb124038967daa96420f45bc
tickle <qu7.52m@gmail.com>
2021-09-16 23:45:04 +0900

    [ver23.1.1] フリーズアローの挙動見直し、Boost時の軌道計算見直し

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2021/09/11
+ * Revised : 2021/09/16
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 23.1.0`;
-const g_revisedDate = `2021/09/11`;
+const g_version = `Ver 23.1.1`;
+const g_revisedDate = `2021/09/16`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
