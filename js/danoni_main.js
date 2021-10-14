2804e0bd63b8035a30549e03ab35fd1cf77c601b
tickle <qu7.52m@gmail.com>
2021-10-14 23:05:35 +0900

    [ver23.5.0] 譜面変更時、キーが同一の場合に設定を一部引き継ぐよう変更　他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2021/10/04
+ * Revised : 2021/10/14
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 23.4.1`;
-const g_revisedDate = `2021/10/04`;
+const g_version = `Ver 23.5.0`;
+const g_revisedDate = `2021/10/14`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
