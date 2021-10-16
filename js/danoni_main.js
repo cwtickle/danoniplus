48aeae608e19bd9511ad92d252ce020642c10ac5
tickle <qu7.52m@gmail.com>
2021-10-16 18:49:58 +0900

    [ver23.5.1] 保存したキーコンフィグが無いときに設定画面で止まることがある問題を修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2021/10/14
+ * Revised : 2021/10/16
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 23.5.0`;
-const g_revisedDate = `2021/10/14`;
+const g_version = `Ver 23.5.1`;
+const g_revisedDate = `2021/10/16`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
