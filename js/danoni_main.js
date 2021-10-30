09db2da63042c41d750197c6c8956a2989ac9c12
tickle <qu7.52m@gmail.com>
2021-10-30 16:30:35 +0900

    [0471/warning-window:0.1.0] Firefox時、警告ウィンドウが最小化される問題を修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -2668,7 +2668,7 @@ function setWindowStyle(_text, _bkColor, _textColor, _align = C_ALIGN_LEFT) {
 	range.selectNode(tmplbl);
 
 	// ウィンドウ枠の行を元に縦の長さを決定(150pxを超えた場合は縦スクロールバーを付与)
-	const len = (range.getClientRects().length) * 21;
+	const len = (Math.max(range.getClientRects().length, _text.split(`<br>`).length + _text.split(`<p>`).length - 1)) * 21;
 	const warnHeight = (len < 150 ? len : 150);
 	const lbl = createDivCss2Label(`lblWarning`, _text, {
 		x: 0, y: 70, w: g_sWidth, h: warnHeight, siz: C_SIZ_MAIN, backgroundColor: _bkColor,
