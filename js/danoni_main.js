1a070844ce197222f410ce4fe4f450097e8fc0e7
tickle <qu7.52m@gmail.com>
2021-12-13 18:02:57 +0900

    [ver24.5.1] シャッフルグループ・カラーグループに関する挙動不具合を修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2021/12/11
+ * Revised : 2021/12/13
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 24.5.0`;
-const g_revisedDate = `2021/12/11`;
+const g_version = `Ver 24.5.1`;
+const g_revisedDate = `2021/12/13`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
