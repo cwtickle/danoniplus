70932d1262fcf2d403e74c93557fbbe63adc6230
tickle <qu7.52m@gmail.com>
2022-01-19 21:32:24 +0900

    [ver25.3.3] skinTypeがDefaultのとき、divBackが作成されない場合がある問題を修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2022/01/18
+ * Revised : 2022/01/19
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 25.3.2`;
-const g_revisedDate = `2022/01/18`;
+const g_version = `Ver 25.3.3`;
+const g_revisedDate = `2022/01/19`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
