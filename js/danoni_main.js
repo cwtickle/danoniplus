e200a994030ea3cf028a13f6403cbcfa3b8ae883
tickle <qu7.52m@gmail.com>
2022-01-22 09:51:44 +0900

    [ver25.4.0] back/maskデータで画像パスのカレントディレクトリ指定に対応　他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2022/01/19
+ * Revised : 2022/01/22
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 25.3.3`;
-const g_revisedDate = `2022/01/19`;
+const g_version = `Ver 25.4.0`;
+const g_revisedDate = `2022/01/22`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
--- js/lib/danoni_constants.js
+++ js/lib/danoni_constants.js
@@ -5,7 +5,7 @@
  *
  * Source by tickle
  * Created : 2019/11/19
- * Revised : 2022/01/16 (v25.3.0)
+ * Revised : 2022/01/22 (v25.4.0)
  *
  * https://github.com/cwtickle/danoniplus
  */
