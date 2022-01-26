37b104a8179321a8af6f05f2b133549610bd52e9
tickle <qu7.52m@gmail.com>
2022-01-26 11:40:55 +0900

    [ver25.5.0] 推定Adj実装、矢印色変化に対するフリーズアローの追従設定を一部分離　他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2022/01/22
+ * Revised : 2022/01/26
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 25.4.1`;
-const g_revisedDate = `2022/01/22`;
+const g_version = `Ver 25.5.0`;
+const g_revisedDate = `2022/01/26`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
--- js/lib/danoni_constants.js
+++ js/lib/danoni_constants.js
@@ -5,7 +5,7 @@
  *
  * Source by tickle
  * Created : 2019/11/19
- * Revised : 2022/01/22 (v25.4.0)
+ * Revised : 2022/01/26 (v25.5.0)
  *
  * https://github.com/cwtickle/danoniplus
  */
