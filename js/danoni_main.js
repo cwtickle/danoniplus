297da5d71757579480c0ac956609847c6e2f92e0
tickle <qu7.52m@gmail.com>
2022-01-29 18:08:52 +0900

    [ver25.5.2] シャッフル・カラーグループを変更した際にその内容が反映されない問題を修正　他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2022/01/26
+ * Revised : 2022/01/29
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 25.5.1`;
-const g_revisedDate = `2022/01/26`;
+const g_version = `Ver 25.5.2`;
+const g_revisedDate = `2022/01/29`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
