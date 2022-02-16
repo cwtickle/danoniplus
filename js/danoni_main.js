601b219dac68916fe44c676ee3be07fdee16e52c
tickle <qu7.52m@gmail.com>
2022-02-16 21:17:57 +0900

    [ver19.5.14] npm環境の整備

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2021/10/27
+ * Revised : 2022/02/16
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 19.5.13`;
-const g_revisedDate = `2021/10/27`;
+const g_version = `Ver 19.5.14`;
+const g_revisedDate = `2022/02/16`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
