e928516df501a4fe010d2a63d1bee1b954693863
tickle <qu7.52m@gmail.com>
2022-02-23 14:32:03 +0900

    [ver26.3.1] メッセージインポート部分のコード整理

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -8,7 +8,7 @@
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 26.3.0`;
+const g_version = `Ver 26.3.1`;
 const g_revisedDate = `2022/02/23`;
 const g_alphaVersion = ``;
 
