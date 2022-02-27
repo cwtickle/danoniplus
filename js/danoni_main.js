d80b52ca47904a9eb09dd32adf3787c460ac0224
tickle <qu7.52m@gmail.com>
2022-02-27 21:47:59 +0900

    [ver26.5.0] カスタムキー定義のpos項目について小数点に対応　他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2022/02/25
+ * Revised : 2022/02/27
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 26.4.0`;
-const g_revisedDate = `2022/02/25`;
+const g_version = `Ver 26.5.0`;
+const g_revisedDate = `2022/02/27`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
