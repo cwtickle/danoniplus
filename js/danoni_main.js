447033e73ba574384b5fde05464a1eacc92e66db
tickle <qu7.52m@gmail.com>
2022-03-06 18:54:14 +0900

    [ver26.6.1] マターリを出すとキーが利かなくなる問題を修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2022/03/05
+ * Revised : 2022/03/06
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 26.6.0`;
-const g_revisedDate = `2022/03/05`;
+const g_version = `Ver 26.6.1`;
+const g_revisedDate = `2022/03/06`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
