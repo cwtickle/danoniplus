fb55cbaf0799917ac0497bfe77e490fa84517a52
tickle <qu7.52m@gmail.com>
2022-05-22 18:00:21 +0900

    [ver27.5.1] カレントパス取得部分のURIエンコード処理を元に戻す

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2022/05/21
+ * Revised : 2022/05/22
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 27.5.0`;
-const g_revisedDate = `2022/05/21`;
+const g_version = `Ver 27.5.1`;
+const g_revisedDate = `2022/05/22`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
