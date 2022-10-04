ecce9caecb53992d1f916f7486aa88867c34fe4b
tickle <qu7.52m@gmail.com>
2022-10-04 12:19:34 +0900

    [ver28.2.0] カスタムキーのカラー・シャッフルグループの実装　他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2022/10/02
+ * Revised : 2022/10/04
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 28.1.2`;
-const g_revisedDate = `2022/10/02`;
+const g_version = `Ver 28.2.0`;
+const g_revisedDate = `2022/10/04`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
