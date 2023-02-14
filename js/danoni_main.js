41d85112e681b243f54960ff7974e258143f4d4b
tickle <qu7.52m@gmail.com>
2023-02-14 22:26:19 +0900

    [ver30.1.1] 背景・マスクモーションの座標系で変数が扱えるよう変更

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -8,7 +8,7 @@
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 30.1.0`;
+const g_version = `Ver 30.1.1`;
 const g_revisedDate = `2023/02/14`;
 const g_alphaVersion = ``;
 
