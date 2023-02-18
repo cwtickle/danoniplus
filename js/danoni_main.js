433cebcf86f8a1cffa8a3c94d67e54a998c022af
tickle <qu7.52m@gmail.com>
2023-02-18 15:23:55 +0900

    [ver30.2.1] フェードインでスクロール反転の初期位置が正しく反映されない問題を修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2023/02/17
+ * Revised : 2023/02/18
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 30.2.0`;
-const g_revisedDate = `2023/02/17`;
+const g_version = `Ver 30.2.1`;
+const g_revisedDate = `2023/02/18`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
