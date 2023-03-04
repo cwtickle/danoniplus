ff605d87c42affdc182bf46a7fac217c047a7683
tickle <qu7.52m@gmail.com>
2023-03-04 21:39:06 +0900

    [ver24.6.16] 11, 11Lkeyの別キーモードでFlatにしたときの設定誤りを修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2023/02/26
+ * Revised : 2023/03/04
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 24.6.15`;
-const g_revisedDate = `2023/02/26`;
+const g_version = `Ver 24.6.16`;
+const g_revisedDate = `2023/03/04`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
