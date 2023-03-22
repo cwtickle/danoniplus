bab21c458880373312bd85fa7cf2268c419c506a
tickle <qu7.52m@gmail.com>
2023-03-22 23:23:23 +0900

    [ver31.0.1] 譜面リスト中、上下キーを使って選択した譜面がキー数違いのときの問題を修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2023/03/20
+ * Revised : 2023/03/22
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 31.0.0`;
-const g_revisedDate = `2023/03/20`;
+const g_version = `Ver 31.0.1`;
+const g_revisedDate = `2023/03/22`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
