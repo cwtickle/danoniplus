f1f23b75f734f69cc2619be9b36b903bdb4c4d77
tickle <qu7.52m@gmail.com>
2023-04-08 09:59:05 +0900

    [ver31.4.0] キー割当設定についてキー名（コード名）が使えるよう変更

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2023/04/05
+ * Revised : 2023/04/08
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 31.3.1`;
-const g_revisedDate = `2023/04/05`;
+const g_version = `Ver 31.4.0`;
+const g_revisedDate = `2023/04/08`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
--- js/lib/danoni_constants.js
+++ js/lib/danoni_constants.js
@@ -5,7 +5,7 @@
  *
  * Source by tickle
  * Created : 2019/11/19
- * Revised : 2023/04/05 (v31.3.1)
+ * Revised : 2023/04/08 (v31.4.0)
  *
  * https://github.com/cwtickle/danoniplus
  */
