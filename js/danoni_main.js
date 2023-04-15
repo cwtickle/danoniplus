13c0667b87339b9812e5042d028f51fa315c9ca5
tickle <qu7.52m@gmail.com>
2023-04-15 18:37:57 +0900

    [ver31.4.1] カスタムキーのposXに空が指定されたときに初期化されるよう処理を改善

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2023/04/08
+ * Revised : 2023/04/15
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 31.4.0`;
-const g_revisedDate = `2023/04/08`;
+const g_version = `Ver 31.4.1`;
+const g_revisedDate = `2023/04/15`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
