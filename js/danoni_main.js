6dde2673bdecde26a97e88ea20594e8a9639a47d
tickle <qu7.52m@gmail.com>
2023-04-26 23:08:02 +0900

    [ver31.6.0] 設定系のサブ関数をグローバル関数に変更　他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2023/04/16
+ * Revised : 2023/04/26
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 31.5.0`;
-const g_revisedDate = `2023/04/16`;
+const g_version = `Ver 31.6.0`;
+const g_revisedDate = `2023/04/26`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
