af11db0058be960b4bd276299d0922d84114f889
tickle <qu7.52m@gmail.com>
2023-06-09 22:33:09 +0900

    [ver32.4.0] ゲージ詳細部分の仕様変更

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2023/06/02
+ * Revised : 2023/06/09
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 32.3.0`;
-const g_revisedDate = `2023/06/02`;
+const g_version = `Ver 32.4.0`;
+const g_revisedDate = `2023/06/09`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
