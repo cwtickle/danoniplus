a59c57d2c8b0477532a76b7740798cdaa984d7b8
tickle <44026291+cwtickle@users.noreply.github.com>
2023-06-24 18:47:10 +0900

    [ver32.5.0] 空押し判定の実装、譜面明細部分の表示見直し

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2023/06/09
+ * Revised : 2023/06/24
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 32.4.0`;
-const g_revisedDate = `2023/06/09`;
+const g_version = `Ver 32.5.0`;
+const g_revisedDate = `2023/06/24`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
