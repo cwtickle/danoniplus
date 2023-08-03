ac7ffface9e6295b5b2c1892d373bd45e8e804ad
tickle <44026291+cwtickle@users.noreply.github.com>
2023-08-03 23:13:24 +0900

    [ver33.1.0] 結果画面のSNS投稿用画像コピー機能を実装 他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2023/07/29
+ * Revised : 2023/08/03
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 33.0.0`;
-const g_revisedDate = `2023/07/29`;
+const g_version = `Ver 33.1.0`;
+const g_revisedDate = `2023/08/03`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
--- js/lib/danoni_constants.js
+++ js/lib/danoni_constants.js
@@ -5,7 +5,7 @@
  *
  * Source by tickle
  * Created : 2019/11/19
- * Revised : 2023/07/29 (v33.0.0)
+ * Revised : 2023/08/03 (v33.1.0)
  *
  * https://github.com/cwtickle/danoniplus
  */
