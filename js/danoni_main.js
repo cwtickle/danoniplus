5ed08479997d31417d0dec5afeab622544860b78
tickle <44026291+cwtickle@users.noreply.github.com>
2023-08-04 07:44:18 +0900

    [ver33.1.1] 背景・マスクモーションのフレーム計算誤りを修正　他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2023/08/03
+ * Revised : 2023/08/04
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 33.1.0`;
-const g_revisedDate = `2023/08/03`;
+const g_version = `Ver 33.1.1`;
+const g_revisedDate = `2023/08/04`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
