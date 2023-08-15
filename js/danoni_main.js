9f5d9c9a9da00ff0856012fc6cab1405d6449123
tickle <44026291+cwtickle@users.noreply.github.com>
2023-08-15 17:49:40 +0900

    [ver31.1.3] リザルト画像コピー用のテキストのエスケープ文字が戻されていない問題を修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2023/08/14
+ * Revised : 2023/08/15
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 33.1.2`;
-const g_revisedDate = `2023/08/14`;
+const g_version = `Ver 33.1.3`;
+const g_revisedDate = `2023/08/15`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
