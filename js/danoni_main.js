7e37321ed7571cb02eabd7eddd3ecbd54ba7101f
tickle <44026291+cwtickle@users.noreply.github.com>
2023-08-20 15:59:23 +0900

    [ver33.2.0] 画面の上下に共通の枠線を一律追加　他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2023/08/19
+ * Revised : 2023/08/20
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 33.1.4`;
-const g_revisedDate = `2023/08/19`;
+const g_version = `Ver 33.2.0`;
+const g_revisedDate = `2023/08/20`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
