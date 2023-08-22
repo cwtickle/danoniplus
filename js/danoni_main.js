b7669b1cb48df8e34fc962f4343c43d608933fa6
tickle <44026291+cwtickle@users.noreply.github.com>
2023-08-22 23:25:32 +0900

    [ver33.3.0] リザルト画像用にカスタム変数を追加できるよう対応 他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2023/08/20
+ * Revised : 2023/08/22
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 33.2.0`;
-const g_revisedDate = `2023/08/20`;
+const g_version = `Ver 33.3.0`;
+const g_revisedDate = `2023/08/22`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
