dc97952031c8ad0acce60a9a0079cefd163a9ae6
tickle <44026291+cwtickle@users.noreply.github.com>
2023-08-27 17:59:24 +0900

    [ver33.4.0] ショートカットキーが通常と異なる場合の表示とその表示エリアの設定を追加 他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2023/08/22
+ * Revised : 2023/08/27
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 33.3.0`;
-const g_revisedDate = `2023/08/22`;
+const g_version = `Ver 33.4.0`;
+const g_revisedDate = `2023/08/27`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
