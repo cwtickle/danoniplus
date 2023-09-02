053496b63cfb314ffc072148299068b5d00c3270
tickle <44026291+cwtickle@users.noreply.github.com>
2023-09-02 13:29:17 +0900

    [ver31.7.7] エラー時に表示される音楽ファイルの参照先URLが実際と異なる問題を修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2023/08/20
+ * Revised : 2023/09/02
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 31.7.6`;
-const g_revisedDate = `2023/08/20`;
+const g_version = `Ver 31.7.7`;
+const g_revisedDate = `2023/09/02`;
 const g_alphaVersion = ``;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
@@ -6725,7 +6725,7 @@ const loadMusic = _ => {
 			lblLoading.textContent = g_lblNameObj.pleaseWait;
 			setAudio(blobUrl);
 		} else {
-			makeWarningWindow(`${g_msgInfoObj.E_0041.split('{0}').join(getFullPath(musicUrl))}<br>(${request.status} ${request.statusText})`, { backBtnUse: true });
+			makeWarningWindow(`${g_msgInfoObj.E_0041.split('{0}').join(getFullPath(url))}<br>(${request.status} ${request.statusText})`, { backBtnUse: true });
 		}
 	});
 
