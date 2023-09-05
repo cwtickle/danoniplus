6c948c8be6644b722a6576bf8ce7edd179ae88fc
tickle <44026291+cwtickle@users.noreply.github.com>
2023-09-05 22:43:39 +0900

    [ver33.5.1] グラデーション記法のグラデーション外を指定する場合のセパレート文字を見直し

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2023/09/02
+ * Revised : 2023/09/05
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 33.5.0`;
-const g_revisedDate = `2023/09/02`;
+const g_version = `Ver 33.5.1`;
+const g_revisedDate = `2023/09/05`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
