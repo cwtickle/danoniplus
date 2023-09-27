d083089585b89af8bc194d6715284d32ffb13017
tickle <44026291+cwtickle@users.noreply.github.com>
2023-09-27 22:56:24 +0900

    [ver34.1.0] 歌詞表示及びタイトル曲名・アーティスト名表示で、条件付きでカンマが使えるよう変更

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2023/09/24
+ * Revised : 2023/09/27
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 34.0.0`;
-const g_revisedDate = `2023/09/24`;
+const g_version = `Ver 34.1.0`;
+const g_revisedDate = `2023/09/27`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
