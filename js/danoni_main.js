c34853371d3cce12edb878cae6ca38e2af832b2d
tickle <44026291+cwtickle@users.noreply.github.com>
2023-10-15 23:29:02 +0900

    [ver34.3.1] startFrame未指定時に起動に失敗する問題を修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2023/10/13
+ * Revised : 2023/10/15
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 34.3.0`;
-const g_revisedDate = `2023/10/13`;
+const g_version = `Ver 34.3.1`;
+const g_revisedDate = `2023/10/15`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
