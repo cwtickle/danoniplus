409a94ba2cd6f62c9a4304e43083c6c20e23747b
tickle <44026291+cwtickle@users.noreply.github.com>
2023-10-21 09:42:13 +0900

    [ver34.3.2] HitPosition設定が機能していない問題を修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2023/10/15
+ * Revised : 2023/10/21
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 34.3.1`;
-const g_revisedDate = `2023/10/15`;
+const g_version = `Ver 34.3.2`;
+const g_revisedDate = `2023/10/21`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
