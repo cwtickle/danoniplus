e5a71393dd94146145c4473f943ad3492b6dc031
tickle <44026291+cwtickle@users.noreply.github.com>
2023-10-29 10:03:06 +0900

    [ver34.4.0] 環境により右シフトキーが動作しない問題を改善

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2023/10/21
+ * Revised : 2023/10/29
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 34.3.2`;
-const g_revisedDate = `2023/10/21`;
+const g_version = `Ver 34.4.0`;
+const g_revisedDate = `2023/10/29`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
--- js/lib/danoni_constants.js
+++ js/lib/danoni_constants.js
@@ -5,7 +5,7 @@
  *
  * Source by tickle
  * Created : 2019/11/19
- * Revised : 2023/10/01 (v34.2.0)
+ * Revised : 2023/10/29 (v34.4.0)
  *
  * https://github.com/cwtickle/danoniplus
  */
