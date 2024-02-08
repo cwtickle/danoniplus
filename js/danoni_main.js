dea7313c053140746ce8222d30ce8bbc03dfb0ae
tickle <44026291+cwtickle@users.noreply.github.com>
2024-02-08 23:05:36 +0900

    [ver35.2.1] posXの略記記法について下段記法にも対応

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -8,7 +8,7 @@
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 35.2.0`;
+const g_version = `Ver 35.2.1`;
 const g_revisedDate = `2024/02/08`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
