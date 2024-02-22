cc9914842b1a9adbd28a728cb137f11dccb0b74e
tickle <44026291+cwtickle@users.noreply.github.com>
2024-02-22 23:39:13 +0900

    [0782/1.2] キーパターン略記のコメントを一部元に戻す

--- js/lib/danoni_constants.js
+++ js/lib/danoni_constants.js
@@ -1843,6 +1843,7 @@ const g_keyObj = {
     chara8_2: [`sleft`, `left`, `leftdia`, `down`, `space`, `up`, `rightdia`, `right`],
 
     // 頻度の高い譜面データ名パターン
+    // 後で chara4A, chara4A_a, chara4A_b, ... に変換する
     ptchara4A: [`left`, `down`, `up`, `right`],
     ptchara3S: [`left`, `leftdia`, `down`],
     ptchara3J: [`up`, `rightdia`, `right`],
