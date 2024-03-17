c7817b55506a5a3c3f67be40fd319c7dc7f9ca91
tickle <44026291+cwtickle@users.noreply.github.com>
2024-03-17 22:52:51 +0900

    [ver35.4.3] 一つ前のフリーズアローの判定条件の問題を修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2024/02/29
+ * Revised : 2024/03/17
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 35.4.2`;
-const g_revisedDate = `2024/02/29`;
+const g_version = `Ver 35.4.3`;
+const g_revisedDate = `2024/03/17`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
