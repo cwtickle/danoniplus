2b25079f5d1fa82997ab4e0e288b63bae3c99622
tickle <44026291+cwtickle@users.noreply.github.com>
2024-05-01 20:35:06 +0900

    [ver36.2.0] 譜面明細画面にハイスコア表示を実装　他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2024/04/18
+ * Revised : 2024/05/01
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 36.1.0`;
-const g_revisedDate = `2024/04/18`;
+const g_version = `Ver 36.2.0`;
+const g_revisedDate = `2024/05/01`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
