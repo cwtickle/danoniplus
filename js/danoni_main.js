fb2fa6f2d23600a9864c10635f9c2081afc812a7
tickle <44026291+cwtickle@users.noreply.github.com>
2024-05-02 22:15:59 +0900

    [ver36.2.1] Accuracyが計算外のときに結果画面でエラーになることがある問題を修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2024/05/01
+ * Revised : 2024/05/02
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 36.2.0`;
-const g_revisedDate = `2024/05/01`;
+const g_version = `Ver 36.2.1`;
+const g_revisedDate = `2024/05/02`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
