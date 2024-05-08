80d0b1831d1742dc9598b9b03a7b14d7b982887c
tickle <44026291+cwtickle@users.noreply.github.com>
2024-05-08 22:55:16 +0900

    [ver36.3.0] カラーグループ数を5から10に拡張　他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2024/05/02
+ * Revised : 2024/05/08
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 36.2.1`;
-const g_revisedDate = `2024/05/02`;
+const g_version = `Ver 36.3.0`;
+const g_revisedDate = `2024/05/08`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
