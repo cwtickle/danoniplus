a40fd5ec55f756a46a9110dbd3e942a026a49592
tickle <44026291+cwtickle@users.noreply.github.com>
2024-06-08 17:06:03 +0900

    [ver36.6.0] シャッフルグループ数の上限を見直し、ミラー譜面のハイスコア保存条件を変更　他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2024/06/01
+ * Revised : 2024/06/08
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 36.5.0`;
-const g_revisedDate = `2024/06/05`;
+const g_version = `Ver 36.6.0`;
+const g_revisedDate = `2024/06/08`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
--- js/lib/danoni_constants.js
+++ js/lib/danoni_constants.js
@@ -5,7 +5,7 @@
  *
  * Source by tickle
  * Created : 2019/11/19
- * Revised : 2024/06/05 (v36.5.0)
+ * Revised : 2024/06/08 (v36.6.0)
  *
  * https://github.com/cwtickle/danoniplus
  */
