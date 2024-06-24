ea07c07e82d2dba59be0cd53dfc308f56c5ade74
tickle <44026291+cwtickle@users.noreply.github.com>
2024-06-25 07:12:59 +0900

    [ver35.5.4] 速度変化平均（AvgO, AvgB）の表示誤差を修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2024/06/20
+ * Revised : 2024/06/25
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 35.5.3`;
-const g_revisedDate = `2024/06/20`;
+const g_version = `Ver 35.5.4`;
+const g_revisedDate = `2024/06/25`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
@@ -4755,7 +4755,7 @@ const drawSpeedGraph = _scoreId => {
 	drawBaseLine(context);
 
 	const avgX = [0, 0];
-	const avgSubX = [1, 1];
+	const avgSubX = [0, 0];
 	const lineX = [125, 210];
 	Object.keys(speedObj).forEach((speedType, j) => {
 		context.beginPath();
