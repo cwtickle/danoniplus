9183e6ab0c100b50a2314e9bb5940e07c69c5902
tickle <44026291+cwtickle@users.noreply.github.com>
2024-06-29 16:40:55 +0900

    [ver37.2.0] playbackRateの値に合わせてAdjustmentの設定幅を伸縮するよう変更

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2024/06/25
+ * Revised : 2024/06/29
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 37.1.0`;
-const g_revisedDate = `2024/06/25`;
+const g_version = `Ver 37.2.0`;
+const g_revisedDate = `2024/06/29`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
--- js/lib/danoni_constants.js
+++ js/lib/danoni_constants.js
@@ -5,7 +5,7 @@
  *
  * Source by tickle
  * Created : 2019/11/19
- * Revised : 2024/06/25 (v37.1.0)
+ * Revised : 2024/06/29 (v37.2.0)
  *
  * https://github.com/cwtickle/danoniplus
  */
