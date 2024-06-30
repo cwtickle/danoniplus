ee6444a4dd7f0ec495de2cdd57fb96c47e4d8cb9
tickle <44026291+cwtickle@users.noreply.github.com>
2024-06-30 22:17:54 +0900

    [ver37.2.1] mask, background-clipについてベンダープレフィックスの互換設定を元に戻す

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2024/06/29
+ * Revised : 2024/06/30
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 37.2.0`;
-const g_revisedDate = `2024/06/29`;
+const g_version = `Ver 37.2.1`;
+const g_revisedDate = `2024/06/30`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
