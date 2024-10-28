fb6d6b78b3f2c69e80b53c14c31e0bc41d9e1bab
tickle <44026291+cwtickle@users.noreply.github.com>
2024-10-28 23:04:25 +0900

    [ver37.7.1] 色変化（ncolor_data）で矢印種類を省略したときに色変化対象が正しく反映されない問題を修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2024/10/06
+ * Revised : 2024/10/28
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 37.7.0`;
-const g_revisedDate = `2024/10/06`;
+const g_version = `Ver 37.7.1`;
+const g_revisedDate = `2024/10/28`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
