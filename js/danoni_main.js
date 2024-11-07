55747f6b918bd2ab3f472c4d3d0a7b0f1d8a5774
tickle <44026291+cwtickle@users.noreply.github.com>
2024-11-07 20:28:52 +0900

    [ver37.8.2] 旧関数のボタン作成処理の引数間違いを修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2024/11/04
+ * Revised : 2024/11/07
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 37.8.1`;
-const g_revisedDate = `2024/11/04`;
+const g_version = `Ver 37.8.2`;
+const g_revisedDate = `2024/11/07`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
