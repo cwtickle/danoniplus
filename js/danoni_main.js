907e8465df3454829eb059b8b59ce383622f430b
tickle <qu7.52m@gmail.com>
2024-12-07 15:49:42 +0900

    [ver38.1.1] Fountainで全体変速が1倍未満、個別加速が1倍以上のときにモーションが見づらくなる問題を修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2024/12/02
+ * Revised : 2024/12/07
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 38.1.0`;
-const g_revisedDate = `2024/12/02`;
+const g_version = `Ver 38.1.1`;
+const g_revisedDate = `2024/12/07`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
