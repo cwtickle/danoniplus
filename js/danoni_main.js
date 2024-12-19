979d6b3ee61cb1315677dd529049a1837cfc5f84
tickle <qu7.52m@gmail.com>
2024-12-19 18:54:28 +0900

    [ver38.2.1] 色変化(ncolor_data)でキーパターンを変えると適用する矢印グループが想定と異なる問題を修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2024/12/14
+ * Revised : 2024/12/19
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 38.2.0`;
-const g_revisedDate = `2024/12/14`;
+const g_version = `Ver 38.2.1`;
+const g_revisedDate = `2024/12/19`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
