5c569346bc12416920f14ccb001646727ac2eb4f
tickle <qu7.52m@gmail.com>
2024-12-19 21:25:35 +0900

    [ver38.2.2] 色変化(ncolor_data)で別キーモードにしか存在しないレーンを含む矢印グループ指定ができるよう修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -8,7 +8,7 @@
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 38.2.1`;
+const g_version = `Ver 38.2.2`;
 const g_revisedDate = `2024/12/19`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
