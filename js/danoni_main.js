ddc09d6f28db4bf20a2a630e178e35e0d3ce6e7d
tickle <qu7.52m@gmail.com>
2025-02-11 22:14:00 +0900

    [ver39.4.1] CSSのpointer-event属性のデフォルト値を種別ごとに設定するよう変更　他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2025/02/09
+ * Revised : 2025/02/11
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 39.4.0`;
-const g_revisedDate = `2025/02/10`;
+const g_version = `Ver 39.4.1`;
+const g_revisedDate = `2025/02/11`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
--- js/lib/danoni_constants.js
+++ js/lib/danoni_constants.js
@@ -5,7 +5,7 @@
  *
  * Source by tickle
  * Created : 2019/11/19
- * Revised : 2025/02/10 (v39.4.0)
+ * Revised : 2025/02/11 (v39.4.1)
  *
  * https://github.com/cwtickle/danoniplus
  */
