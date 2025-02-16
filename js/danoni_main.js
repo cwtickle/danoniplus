45e3e305b33047c683dbe86f5a8ebb8408998b07
tickle <qu7.52m@gmail.com>
2025-02-16 23:12:50 +0900

    [ver39.6.0] ローカルストレージ情報を使わないセーフモードの実装　他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2025/02/15
+ * Revised : 2025/02/16
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 39.5.0`;
-const g_revisedDate = `2025/02/15`;
+const g_version = `Ver 39.6.0`;
+const g_revisedDate = `2025/02/16`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
