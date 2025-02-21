9f4954c6e51df601275f3474162f5af09986c013
tickle <qu7.52m@gmail.com>
2025-02-21 19:30:00 +0900

    [ver39.8.0] ローカルストレージ情報のコピー機能を追加

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2025/02/20
+ * Revised : 2025/02/21
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 39.7.2`;
-const g_revisedDate = `2025/02/20`;
+const g_version = `Ver 39.8.0`;
+const g_revisedDate = `2025/02/21`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
--- js/lib/danoni_constants.js
+++ js/lib/danoni_constants.js
@@ -5,7 +5,7 @@
  *
  * Source by tickle
  * Created : 2019/11/19
- * Revised : 2025/02/20 (v39.7.2)
+ * Revised : 2025/02/21 (v39.8.0)
  *
  * https://github.com/cwtickle/danoniplus
  */
