6d94ca72c668b8fb9dc3ae51dc1be23d7bb76072
tickle <qu7.52m@gmail.com>
2025-04-16 20:53:34 +0900

    [ver41.0.1] difDataで製作者名を含む場合にタイトル画面のMaker表記が想定と異なることがある問題を修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2025/04/12
+ * Revised : 2025/04/16
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 41.0.0`;
-const g_revisedDate = `2025/04/12`;
+const g_version = `Ver 41.0.1`;
+const g_revisedDate = `2025/04/16`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
