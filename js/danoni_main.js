5b831cc52160adc955cdb920597d07094d18cc09
tickle <qu7.52m@gmail.com>
2025-05-15 21:00:59 +0900

    change; レビュー内容を反映

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -3464,8 +3464,8 @@ const headerConvert = _dosObj => {
 
 		const lastIdx = Math.max(...obj.musicNos, musicData.length - 1);
 		for (let j = 0; j <= lastIdx; j++) {
-			musicData[j] ??= ``;
-			const musics = splitComma(musicData[j]);
+			const tmpMusicData = musicData[j] ?? ``;
+			const musics = splitComma(tmpMusicData);
 
 			obj.musicTitles[j] = hasVal(musics[0])
 				? escapeHtml(getMusicNameSimple(musics[0]))
