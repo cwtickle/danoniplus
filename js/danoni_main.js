a44668e2e65deffd3bc67893f01d9542c4279632
tickle <qu7.52m@gmail.com>
2025-05-17 17:40:59 +0900

    [ver41.4.0] 必要以上にキャッシュクリアを行わないよう処理を見直し

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2025/05/15
+ * Revised : 2025/05/17
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 41.3.2`;
-const g_revisedDate = `2025/05/15`;
+const g_version = `Ver 41.4.0`;
+const g_revisedDate = `2025/05/17`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
