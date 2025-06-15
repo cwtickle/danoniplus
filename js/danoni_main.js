289e37baa71346f3526b3adaecbfadb9b383f31a
tickle <qu7.52m@gmail.com>
2025-06-15 22:43:55 +0900

    [ver42.2.0] キー別の最小横幅の算出方法を見直し　他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2025/06/08
+ * Revised : 2025/06/15
  * 
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 42.1.2`;
-const g_revisedDate = `2025/06/08`;
+const g_version = `Ver 42.2.0`;
+const g_revisedDate = `2025/06/15`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
--- js/lib/danoni_constants.js
+++ js/lib/danoni_constants.js
@@ -5,7 +5,7 @@
  *
  * Source by tickle
  * Created : 2019/11/19
- * Revised : 2025/05/24 (v42.0.0)
+ * Revised : 2025/06/15 (v42.2.0)
  *
  * https://github.com/cwtickle/danoniplus
  */
