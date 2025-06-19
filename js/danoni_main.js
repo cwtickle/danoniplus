17f9f08954f4ba3436765364e6e2fc98fed64442
tickle <qu7.52m@gmail.com>
2025-06-20 07:06:09 +0900

    [ver42.2.3] 曲個別のコメントと作品全体のコメントが共存できるよう変更

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2025/06/18
+ * Revised : 2025/06/20
  *
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 42.2.2`;
-const g_revisedDate = `2025/06/18`;
+const g_version = `Ver 42.2.3`;
+const g_revisedDate = `2025/06/20`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
