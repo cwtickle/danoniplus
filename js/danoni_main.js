8addf23bc99de8c9fb014baf19cdf5dc3130f0e8
tickle <qu7.52m@gmail.com>
2025-08-27 21:22:07 +0900

    [ver42.5.2] 全体色変化と個別色変化が短いスパンで変わるとき、前の色に戻ることがある問題を修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2025/08/25
+ * Revised : 2025/08/27
  *
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 42.5.1`;
-const g_revisedDate = `2025/08/25`;
+const g_version = `Ver 42.5.2`;
+const g_revisedDate = `2025/08/27`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
