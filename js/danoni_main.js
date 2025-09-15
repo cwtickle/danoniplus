8fab14de6ca73a962c60180d9da7c8076af08b8b
tickle <qu7.52m@gmail.com>
2025-09-15 15:24:07 +0900

    [ver43.0.0] 譜面毎の空押し設定に対応

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2025/08/31
+ * Revised : 2025/09/15
  *
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 42.5.3`;
-const g_revisedDate = `2025/08/31`;
+const g_version = `Ver 43.0.0`;
+const g_revisedDate = `2025/09/15`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
