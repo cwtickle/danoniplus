28460e3540ab39190e6766e8a449213d7a4ffffc
tickle <qu7.52m@gmail.com>
2026-02-20 07:24:00 +0900

    [ver44.3.0] FrzReturn設定適用時、回転に奥行きを付けるよう変更　他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2026/02/18
+ * Revised : 2026/02/20
  *
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 44.2.0`;
-const g_revisedDate = `2026/02/18`;
+const g_version = `Ver 44.3.0`;
+const g_revisedDate = `2026/02/20`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
