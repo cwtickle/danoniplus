dfe7ff7a738289defb974b999f9c4eb4d469ddbe
tickle <qu7.52m@gmail.com>
2026-02-21 20:51:33 +0900

    [ver44.4.0] フィルターバーについて階層グループが多い場合の機能改善 他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2026/02/20
+ * Revised : 2026/02/21
  *
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 44.3.0`;
-const g_revisedDate = `2026/02/20`;
+const g_version = `Ver 44.4.0`;
+const g_revisedDate = `2026/02/21`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
