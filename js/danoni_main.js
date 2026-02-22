9d65b666f0fdfcaca55b94a16eed4dde86fbe561
tickle <qu7.52m@gmail.com>
2026-02-23 00:25:01 +0900

    [ver44.5.0] 設定内容のサマリーを確認できる機能を追加 他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2026/02/21
+ * Revised : 2026/02/23
  *
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 44.4.1`;
-const g_revisedDate = `2026/02/21`;
+const g_version = `Ver 44.5.0`;
+const g_revisedDate = `2026/02/23`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
--- js/lib/danoni_constants.js
+++ js/lib/danoni_constants.js
@@ -5,7 +5,7 @@
  *
  * Source by tickle
  * Created : 2019/11/19
- * Revised : 2026/02/21 (v44.4.0)
+ * Revised : 2026/02/23 (v44.5.0)
  *
  * https://github.com/cwtickle/danoniplus
  */
