d05cd619a0c3307630499ede78f3352a8e27289f
tickle <qu7.52m@gmail.com>
2026-02-24 21:06:44 +0900

    [ver44.5.3] g_presetObj.lblRenamesのページ名プロパティが未指定の場合に元の名前が表示される問題を修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2026/02/23
+ * Revised : 2026/02/24
  *
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 44.5.2`;
-const g_revisedDate = `2026/02/23`;
+const g_version = `Ver 44.5.3`;
+const g_revisedDate = `2026/02/24`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
