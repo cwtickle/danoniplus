0e15111db31d16d40fa67b66b205cfe5d94b7c9b
tickle <qu7.52m@gmail.com>
2026-03-12 09:08:51 +0900

    [ver45.5.3] ハイスコア表示でのCopyResultでExcessiveが前回のものを取得してしまう問題を修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2026/03/10
+ * Revised : 2026/03/12
  *
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 45.5.2`;
-const g_revisedDate = `2026/03/10`;
+const g_version = `Ver 45.5.3`;
+const g_revisedDate = `2026/03/12`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
@@ -7359,13 +7359,14 @@ const makeHighScore = _scoreId => {
 			tweetDifData += `:${getStgDetailName(g_stateObj.shuffle)}`;
 		}
 
+		const storedExcessive = g_localStorage.highscores[scoreName]?.excessive;
 		const resultParams = {
 			hashTag: (hasVal(g_headerObj.hashTag) ? ` ${g_headerObj.hashTag}` : ``),
 			tuning: g_headerObj.creatorNames[_scoreId],
 			rankMark: g_localStorage.highscores?.[scoreName]?.rankMark || `--`,
 			playStyleData: g_localStorage.highscores[scoreName]?.playStyle || `--`,
 			highscore: g_localStorage.highscores[scoreName],
-			tweetExcessive: hasVal(g_localStorage.highscores[scoreName]?.excessive, `---`) ? `(+${g_resultObj.excessive})` : ``,
+			tweetExcessive: hasVal(storedExcessive, `---`) ? `(+${storedExcessive})` : ``,
 			musicTitle, tweetDifData, tweetFrzJdg, tweetMaxCombo, baseTwitUrl,
 		};
 		const resultCommon = unEscapeHtml(makeResultText(g_templateObj.resultFormatDf, resultParams));
