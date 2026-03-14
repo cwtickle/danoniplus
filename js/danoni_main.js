fd2b7e77e7e922d98b49066dc968a76c6f301bbb
tickle <qu7.52m@gmail.com>
2026-03-14 22:35:42 +0900

    [ver46.0.1] Reverse設定のマウスオーバー時に他の設定の説明が入り込む問題を修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2026/03/12
+ * Revised : 2026/03/14
  *
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 46.0.0`;
-const g_revisedDate = `2026/03/12`;
+const g_version = `Ver 46.0.1`;
+const g_revisedDate = `2026/03/14`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
