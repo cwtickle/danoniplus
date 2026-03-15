c9ef00e253253cd81c5086bed9f73e963e800f3a
tickle <qu7.52m@gmail.com>
2026-03-15 22:25:15 +0900

    [ver45.5.5] Gauge設定のAccuracyの説明文誤りを修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2026/03/14
+ * Revised : 2026/03/15
  *
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 45.5.4`;
-const g_revisedDate = `2026/03/14`;
+const g_version = `Ver 45.5.5`;
+const g_revisedDate = `2026/03/15`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
