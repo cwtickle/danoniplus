5139a57dc4ba20c44d908ae649ac64768822cf18
tickle <qu7.52m@gmail.com>
2026-03-19 09:49:30 +0900

    [ver46.2.0] Swapping設定のグループ間入れ替え方法を一部変更 他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2026/03/15
+ * Revised : 2026/03/19
  *
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 46.1.0`;
-const g_revisedDate = `2026/03/15`;
+const g_version = `Ver 46.2.0`;
+const g_revisedDate = `2026/03/19`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
