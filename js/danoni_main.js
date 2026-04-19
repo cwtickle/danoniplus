9eccc4915f740195ec3b44efb9685795010e9d3a
tickle <qu7.52m@gmail.com>
2026-04-19 22:19:05 +0900

    [ver47.0.0] 譜面明細画面に譜面内容を表示する機能を追加 他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2026/04/13
+ * Revised : 2026/04/19
  *
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 46.7.0`;
-const g_revisedDate = `2026/04/13`;
+const g_version = `Ver 47.0.0`;
+const g_revisedDate = `2026/04/19`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
