2ae0f9b878240ce70ee5a5190f5876abfef330fc
tickle <qu7.52m@gmail.com>
2026-04-20 09:26:57 +0900

    [ver47.0.1] サイトの設定により譜面ミニマップが左寄せにならないことがある問題を修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2026/04/19
+ * Revised : 2026/04/20
  *
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 47.0.0`;
-const g_revisedDate = `2026/04/19`;
+const g_version = `Ver 47.0.1`;
+const g_revisedDate = `2026/04/20`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
