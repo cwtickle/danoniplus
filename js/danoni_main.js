865bfa3974218bec53e09fd3fa3e6c2c23fede59
tickle <qu7.52m@gmail.com>
2026-04-21 15:13:47 +0900

    [ver47.1.0] カスタムキーが未定義の場合に強制的にデフォルトキーに戻すことで処理を続行させるように変更

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2026/04/20
+ * Revised : 2026/04/21
  *
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 47.0.1`;
-const g_revisedDate = `2026/04/20`;
+const g_version = `Ver 47.1.0`;
+const g_revisedDate = `2026/04/21`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
