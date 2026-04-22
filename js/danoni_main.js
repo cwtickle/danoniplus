674ba60da043aacd6362cb1e87ed27566dfbce41
tickle <qu7.52m@gmail.com>
2026-04-22 12:45:19 +0900

    [ver47.2.0] 譜面切り替え時に現在のスクロール位置（割合）を保持するよう変更

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -8,7 +8,7 @@
  *
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 47.1.1`;
+const g_version = `Ver 47.2.0`;
 const g_revisedDate = `2026/04/22`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
