e785e3d0edf438a07e1faae98759926ed0a281f9
tickle <qu7.52m@gmail.com>
2026-04-25 17:12:30 +0900

    [ver47.3.0] 速度変化(逆走)により矢印の生成・到達の順序が崩れた場合にアラートを出す仕様に変更 他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2026/04/22
+ * Revised : 2026/04/25
  *
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 47.2.0`;
-const g_revisedDate = `2026/04/22`;
+const g_version = `Ver 47.3.0`;
+const g_revisedDate = `2026/04/25`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
--- js/lib/danoni_constants.js
+++ js/lib/danoni_constants.js
@@ -5,7 +5,7 @@
  *
  * Source by tickle
  * Created : 2019/11/19
- * Revised : 2026/04/21 (v47.1.0)
+ * Revised : 2026/04/25 (v47.3.0)
  *
  * https://github.com/cwtickle/danoniplus
  */
