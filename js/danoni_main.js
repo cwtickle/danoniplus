fd02e3664c8d1b1cb33ea3f79018bf4a104a3c1b
tickle <qu7.52m@gmail.com>
2026-05-03 16:38:37 +0900

    [ver47.5.1] 譜面明細子画面のショートカットキーの下キーの初期カーソル位置誤りを修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2026/05/02
+ * Revised : 2026/05/03
  *
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 47.5.0`;
-const g_revisedDate = `2026/05/02`;
+const g_version = `Ver 47.5.1`;
+const g_revisedDate = `2026/05/03`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
