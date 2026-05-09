43a67148b6864c3849acb7755a5844d2e066dc2e
tickle <qu7.52m@gmail.com>
2026-05-09 16:51:10 +0900

    [ver47.5.3] 譜面明細画面のVelocityのショートカットキーが一部機能していない問題を修正 他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2026/05/04
+ * Revised : 2026/05/09
  *
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 47.5.2`;
-const g_revisedDate = `2026/05/04`;
+const g_version = `Ver 47.5.3`;
+const g_revisedDate = `2026/05/09`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
