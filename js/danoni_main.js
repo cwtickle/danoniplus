3f9c251f7490c2c14c8a0881d4a122aa292336e3
tickle <qu7.52m@gmail.com>
2026-05-17 20:49:14 +0900

    [ver48.0.0] 判定系のカスタム関数の引数にレーン番号を追加 他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2026/05/15
+ * Revised : 2026/05/17
  *
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 47.6.4`;
-const g_revisedDate = `2026/05/15`;
+const g_version = `Ver 48.0.0`;
+const g_revisedDate = `2026/05/17`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
--- js/lib/danoni_constants.js
+++ js/lib/danoni_constants.js
@@ -5,7 +5,7 @@
  *
  * Source by tickle
  * Created : 2019/11/19
- * Revised : 2026/05/13 (v47.6.1)
+ * Revised : 2026/05/17 (v48.0.0)
  *
  * https://github.com/cwtickle/danoniplus
  */
