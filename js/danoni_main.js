306d2823fe540010b705b5db290f5675ed5e0a61
tickle <qu7.52m@gmail.com>
2026-05-20 21:19:51 +0900

    [ver48.1.0] Display設定のプレビュー機能を追加、判定キャラクタ位置の変更に対応 他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2026/05/17
+ * Revised : 2026/05/20
  *
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 48.0.0`;
-const g_revisedDate = `2026/05/17`;
+const g_version = `Ver 48.1.0`;
+const g_revisedDate = `2026/05/20`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
--- js/lib/danoni_constants.js
+++ js/lib/danoni_constants.js
@@ -5,7 +5,7 @@
  *
  * Source by tickle
  * Created : 2019/11/19
- * Revised : 2026/05/17 (v48.0.0)
+ * Revised : 2026/05/20 (v48.1.0)
  *
  * https://github.com/cwtickle/danoniplus
  */
