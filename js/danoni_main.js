d5f1bc491b38b7a9c765ef3e31cd1fc39f9f9601
tickle <qu7.52m@gmail.com>
2026-07-12 20:18:37 +0900

    [ver49.3.0] カスタムゲージ名の別名を指定する機能を追加 他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2026/07/07
+ * Revised : 2026/07/12
  *
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 49.2.0`;
-const g_revisedDate = `2026/07/07`;
+const g_version = `Ver 49.3.0`;
+const g_revisedDate = `2026/07/12`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
