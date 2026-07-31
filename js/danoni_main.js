47e09a2035ad3611b67ee8e2e4789cfc73f26d30
tickle <qu7.52m@gmail.com>
2026-07-31 18:05:34 +0900

    [ver49.3.2] カスタムキー定義のlayerTransXについて、Reverseの反転に対応できていない問題を修正

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2026/07/18
+ * Revised : 2026/07/31
  *
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 49.3.1`;
-const g_revisedDate = `2026/07/18`;
+const g_version = `Ver 49.3.2`;
+const g_revisedDate = `2026/07/31`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
