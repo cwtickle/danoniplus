864812b62fb643ec737340a62e96b83c9b294fc3
tickle <qu7.52m@gmail.com>
2026-08-07 08:04:09 +0900

    [ver49.4.1] npm-to-publish.ymlの更新

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2026/08/06
+ * Revised : 2026/08/07
  *
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 49.4.0`;
-const g_revisedDate = `2026/08/06`;
+const g_version = `Ver 49.4.1`;
+const g_revisedDate = `2026/08/07`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
