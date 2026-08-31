2cede5f81764b51316a040b18a9b74ce8df595fc
tickle <qu7.52m@gmail.com>
2026-08-31 12:54:48 +0900

    fix: キー別の設定値入れ替え処理で、適用不可の値が代入される可能性がある問題を修正

--- js/lib/danoni_constants.js
+++ js/lib/danoni_constants.js
@@ -5293,6 +5293,10 @@ const g_root = {
     get g_headerObj() { return g_headerObj; },
     get g_stateObj() { return g_stateObj; },
     get g_keycons() { return g_keycons; },
+    get g_settings() { return g_settings; },
+    get g_imgObj() { return g_imgObj },
+    get g_judgObj() { return g_judgObj },
+    get g_judgRanges() { return g_judgRanges },
 };
 const getPathVal = _path => _path.split(`.`).reduce((o, k) => o?.[k], g_root);
 const setPathVal = (_path, _value) => {
@@ -5306,7 +5310,7 @@ const setPathVal = (_path, _value) => {
     target[last] = _value;
 };
 
-const g_familyObj = { families: [], currentFamily: null, ownership: {}, snapshot: {}, applied: {}, appliedPaths: new Set() };
+const g_familyObj = { families: [], currentFamily: null, ownership: {}, snapshot: {} };
 
 const g_errorCache = {
     'g_customJsObj.titleEnterFrame': [],
