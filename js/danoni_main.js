8fe59e19df8d9c455f5d7f047eda98cb6d7ea9d3
tickle <qu7.52m@gmail.com>
2026-08-31 18:31:43 +0900

    [ver50.1.2] キー別の設定値入れ替え処理で、適用不可の値が代入される可能性がある問題を修正 他

--- js/danoni_main.js
+++ js/danoni_main.js
@@ -4,12 +4,12 @@
  * 
  * Source by tickle
  * Created : 2018/10/08
- * Revised : 2026/08/30
+ * Revised : 2026/08/31
  *
  * https://github.com/cwtickle/danoniplus
  */
-const g_version = `Ver 50.1.1`;
-const g_revisedDate = `2026/08/30`;
+const g_version = `Ver 50.1.2`;
+const g_revisedDate = `2026/08/31`;
 
 // カスタム用バージョン (danoni_custom.js 等で指定可)
 let g_localVersion = ``;
--- js/lib/danoni_constants.js
+++ js/lib/danoni_constants.js
@@ -5,7 +5,7 @@
  *
  * Source by tickle
  * Created : 2019/11/19
- * Revised : 2026/08/29 (v50.1.0)
+ * Revised : 2026/08/31 (v50.1.2)
  *
  * https://github.com/cwtickle/danoniplus
  */
