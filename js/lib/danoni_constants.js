6873e1ece5aaf7e120d69b2b76a7690cf91d1e7f
tickle <qu7.52m@gmail.com>
2026-09-13 13:07:50 +0900

    [ver44.5.24] makeInfoWindowに引数が無いことにより、無音時のメッセージ位置が想定と異なる問題を修正 他

--- js/lib/danoni_constants.js
+++ js/lib/danoni_constants.js
@@ -5,7 +5,7 @@
  *
  * Source by tickle
  * Created : 2019/11/19
- * Revised : 2026/08/01 (v44.5.21)
+ * Revised : 2026/09/13 (v44.5.24)
  *
  * https://github.com/cwtickle/danoniplus
  */
@@ -1109,6 +1109,7 @@ const g_stateObj = {
     bgmLooped: null,
     bgmFadeIn: null,
     bgmFadeOut: null,
+    bgmRestart: null,
     bgmTimeupdateEvtId: null,
     bgmMuteFlg: false,
 
